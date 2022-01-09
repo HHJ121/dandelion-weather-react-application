854acfc430e218be88e84b3a749b21d89f0c6fad
HHJ121 <dandylioneffect@gmail.com>
2022-01-08 22:03:49 -0600

    Added basic structure and details in Weather component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
 import React from "react";
 import "./App.css";
 
+import Weather from "./Weather";
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Yay</h1>
-      <footer>
-        <a
-          href="https://github.com/HHJ121/dandelion-weather-react-application"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>{" "}
-        by
-        <a href="http://guidedbythe1.com/" target="_blank" rel="noreferrer">
-          {""} Haw-Harn Jiang
-        </a>
-        , hosted on {""}
-        <a
-          href="https://hardcore-pasteur-857caf.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        
+        <Weather />
+        <footer>
+          <a
+            href="https://github.com/HHJ121/dandelion-weather-react-application"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by
+          <a href="http://guidedbythe1.com/" target="_blank" rel="noreferrer">
+            {""} Haw-Harn Jiang
+          </a>
+          , hosted on {""}
+          <a
+            href="https://hardcore-pasteur-857caf.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <input
+          type="search"
+          placeholder="Enter a city"
+          className="form-control"
+          autoComplete="off"
+          autoFocus="on"
+        />
+        <input type="submit" value="Search" className="btn btn-primary" />
+      </form>
+      <h1>Taipei City</h1>
+      <ul>
+        <li>Sunday 07:30</li>
+        <li>Mostly cloudy</li>
+      </ul>
+      <div className="row">
+       
+        <div className="col-4 conditions">
+            <ul>
+            <li>Precipitation: 17%</li>
+            <li>Humidity: 77%</li>
+            <li>Wind: 17 km/hr</li>
+          </ul>
+        </div>
+           <div className="col-3">
+          <img
+            src="https://assets.msn.com/bundles/v1/weather/latest/ClearNightV3.svg"
+            alt="Mostly cloudy"
+          />
+          7°C
+        </div>
+      </div>
+    </div>
+  );
+}
