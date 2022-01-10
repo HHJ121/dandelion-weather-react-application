3ff6fc1c603262311b6299e85c2c5d0eb1efa4db
HHJ121 <dandylioneffect@gmail.com>
2022-01-10 16:29:04 -0600

    Added basic styling to Weather component

--- src/Weather.js
+++ src/Weather.js
@@ -16,25 +16,27 @@ export default function Weather() {
         <input type="submit" value="Search" className="btn btn-primary" />
       </form>
       <h1>Taipei City</h1>
-      <ul>
+      <ul className="overview">
         <li>Sunday 07:30</li>
         <li>Mostly cloudy</li>
       </ul>
       <div className="row">
-       
         <div className="col-4 conditions">
-            <ul>
+          <ul>
             <li>Precipitation: 17%</li>
             <li>Humidity: 77%</li>
             <li>Wind: 17 km/hr</li>
           </ul>
         </div>
-           <div className="col-3">
+
+        <div className="col-4">
           <img
             src="https://assets.msn.com/bundles/v1/weather/latest/ClearNightV3.svg"
             alt="Mostly cloudy"
           />
-          7°C
+
+          <span className="temp">7</span>
+          <span className="unit">°C</span>
         </div>
       </div>
     </div>
