79af0352781a9002415d3fbadb40c58d22972290
HHJ121 <dandylioneffect@gmail.com>
2022-01-15 17:38:57 -0600

    Use useEffect to display the appropriate forecast and updated the code

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import "./WeatherForecast.css";
@@ -7,6 +7,19 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [searchLoaded, setSearchLoaded] = useState(false);
   let [forecastData, setForecastData] = useState(null);
+
+  useEffect(() => {
+    setSearchLoaded(false);
+  }, [props.coordinates]);
+
+  function handleLoad() {
+    let apiKey = "c0d5182ce71bc2be9c80f43da3c8ee07";
+    let lon = props.coordinates.lon;
+    let lat = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleForecast);
+  }
 
   function handleForecast(response) {
     setForecastData(response.data.daily);
@@ -24,19 +37,15 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
-            return null;
           })}
         </div>
       </div>
     );
   } else {
-    let apiKey = "c0d5182ce71bc2be9c80f43da3c8ee07";
-    let lon = props.coordinates.lon;
-    let lat = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleForecast);
+    handleLoad();
 
     return null;
   }
